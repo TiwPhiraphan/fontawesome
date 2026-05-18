254463805ced904e2e450abdbf7b9a4d2fe094e9
TiwPhiraphan <tananphiraphan@outlook.com>
2026-05-18 10:28:37 +0700

    All New Module

---
+++ build.ts
@@ -0,0 +1,19 @@
+import { build, type Options } from 'tsup'
+
+const options: Options = {
+    format: 'esm',
+    dts: true,
+    clean: true,
+    minify: true,
+    bundle: false,
+    splitting: true,
+    external: [
+        'react',
+        '@fortawesome/free-solid-svg-icons',
+        '@fortawesome/free-regular-svg-icons',
+        '@fortawesome/free-brands-svg-icons',
+    ]
+}
+
+await build({ ...options, entry: ['src/*.ts'], outDir: 'dist', })
+await build({ ...options, entry: ['src/bin/*.ts'], outDir: 'dist/bin', dts: false, banner: { js: '#!/usr/bin/env node' } })
