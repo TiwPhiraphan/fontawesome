4b266acf0d35dc7a350c5ed1130a01608b02b317
MrTiwz <tananphiraphan@outlook.com>
2026-05-18 11:22:02 +0700

    Enable bundling for bin files in build process

--- build.ts
+++ build.ts
@@ -16,4 +16,4 @@ const options: Options = {
 }
 
 await build({ ...options, entry: ['src/*.ts'], outDir: 'dist', })
-await build({ ...options, entry: ['src/bin/*.ts'], outDir: 'dist/bin', dts: false, banner: { js: '#!/usr/bin/env node' } })
+await build({ ...options, entry: ['src/bin/*.ts'], outDir: 'dist/bin', dts: false, bundle: true, banner: { js: '#!/usr/bin/env node' } })
